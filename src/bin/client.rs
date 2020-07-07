96ffa304d6a622f3323e83aa3e6cf4686177c123
Jack Rieck <jack.rieck@sendgrid.com>
2020-07-07 16:49:15 -0500

    working client server comms with hello world

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,26 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use byteorder::{ByteOrder, BigEndian};
 
 fn main() {
     let mut stream = TcpStream::connect("localhost:4444").unwrap();
     
-    stream.write(&[1]).unwrap();
+    // write tag
+    stream.write(b"\x00").unwrap();
+
+    // generate some data
+    let data = String::from("hello world");
+    let data_bytes = data.as_bytes();
+
+    // get len of data
+    let len = data_bytes.len();
+    let len_u32 = len as u32;
+    let mut len_buf = [0; 4];
+    
+    // serialize len
+    BigEndian::write_u32(&mut len_buf, len_u32);
+    stream.write(&len_buf).unwrap();
+
+    // write data
+    stream.write(&data_bytes).unwrap();
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use byteorder::{ByteOrder, BigEndian};
 
 fn main() {
     
@@ -11,5 +12,26 @@ fn main() {
 }
 
 fn handle(mut stream: TcpStream) {
-    stream.read(&mut [0; 128]).unwrap();
+
+    // read tag
+    let mut tag_buf = [0; 1];
+    stream.read_exact(&mut tag_buf).unwrap();
+    if tag_buf[0] != b"\x00"[0] && tag_buf.len() != 1 {
+        println!("unknown tag");
+        return
+    }
+
+    // read len
+    let mut len_buf = [0; 4];
+    stream.read_exact(&mut len_buf).unwrap();
+
+    // convert to u32
+    let len = BigEndian::read_u32(&len_buf);
+    let len_usize = len as usize;
+
+    // read data
+    let mut data_buf = vec![0; len_usize];
+    stream.read_exact(&mut data_buf).unwrap();
+
+    println!("{:?}", String::from_utf8(data_buf).unwrap());
 }
