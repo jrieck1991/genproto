68ffe1c0f9250d2d4dd9050203f90cc3eaaddf86
Jack Rieck <jack.rieck@sendgrid.com>
2020-07-11 19:49:55 -0500

    no target, fixed with workspaces

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
+mod server;
+
 fn main() {
     // init server
-    let mut s = genproto::server::Server::new("localhost:4444");
+    let mut s = server::Server::new("localhost:4444");
 
     // start listening for requests
     s.start()
---
+++ server/src/server/mod.rs
@@ -0,0 +1,87 @@
+use std::net::{TcpListener, TcpStream};
+mod store;
+
+pub struct Server {
+    addr: String,
+    store: store::Storage,
+}
+
+impl Server {
+    pub fn new(listen_addr: &str) -> Server {
+        Server {
+            addr: listen_addr.to_string(),
+            store: store::Storage::new(),
+        }
+    }
+
+    pub fn start(self: &mut Self) {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        for stream in listener.incoming() {
+            let stream = match stream {
+                Ok(stream) => stream,
+                Err(e) => {
+                    println!("connection error: {}", e);
+                    continue;
+                }
+            };
+            self.handle(stream);
+        }
+    }
+
+    fn handle(self: &mut Self, mut stream: TcpStream) {
+        loop {
+            // read request from stream
+            let request = match lib::read_stream(&mut stream) {
+                Some(request) => request,
+                None => continue,
+            };
+
+            // action on request
+            self.action(request);
+        }
+    }
+
+    // action on data received
+    fn action(self: &mut Self, req: lib::Request) {
+        let action = req.action.as_str();
+
+        match action {
+            "put" => {
+                println!("put received");
+
+                // get value from data struct
+                let value = match req.data.value {
+                    Some(value) => value,
+                    None => {
+                        println!("put requires value field set");
+                        return;
+                    }
+                };
+
+                // put data in storage
+                self.store.set(
+                    String::from_utf8(req.data.key).unwrap(),
+                    String::from_utf8(value).unwrap(),
+                );
+            }
+            "get" => {
+                println!("get received");
+
+                // get key and return value if found
+                let value = match self.store.get(String::from_utf8(req.data.key).unwrap()) {
+                    Some(value) => value,
+                    None => {
+                        println!("no match found");
+                        return;
+                    }
+                };
+
+                println!("match found, value: {}", value);
+            }
+            _ => {
+                println!("{} didn't match any actions", action);
+            }
+        }
+    }
+}
